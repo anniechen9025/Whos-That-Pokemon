495a3dab04ee537f406df64e1d17f54cb8abc68e
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-05-08 11:50:35 -0700

    making server working

---
+++ controllers/api/pokedexRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Pokemon } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        res.status(200).json(newPokemon);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        res.status(200).json(newPokemon);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        res.status(200).json(newPokemon);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPokemon = await Pokemon.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPokemon);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const pokemonData = await Pokemon.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!pokemonData) {
+            res.status(404).json({ message: 'No project found with this id!' });
+            return;
+        }
+
+        res.status(200).json(pokemonData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
