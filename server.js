ee6411b4c0d4e23b14ba317298c3387a990299da
LiHua Anderson <lihua.anderson@gmail.com>
2021-05-10 18:43:07 -0700

    added more to handlebars templates. changed two routes in pokedexroutes to res.render instead of res.json

--- server.js
+++ server.js
@@ -14,15 +14,14 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -36,7 +35,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
