2a0dde6a77307b059bb52a317c5a60343041cfe7
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-05-12 18:41:07 -0700

    minor fixed

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -3,32 +3,23 @@ const router = require('express').Router();
 const { Pokemon, User } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {  
-      res.render('homepage', {
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  try {
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-  router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
-  router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('signup');
-  });
-  
-  module.exports = router;
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
