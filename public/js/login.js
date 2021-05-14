a83b4eaf773d954cb9d39e3263c9e1ccff3666ab
LiHua Anderson <lihua.anderson@gmail.com>
2021-05-13 20:55:34 -0700

    Consolidated CSS files to style CSS. Fixed Handlebars template for login to show new layout. Added menu handlebar template

--- public/js/login.js
+++ public/js/login.js
@@ -1,67 +1,67 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginForm = document.getElementById('login-form');
+const loginButton = document.getElementById('login-form-submit');
+const loginErrorMsg = document.getElementById('login-error-msg');
 
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Collect values from the login form
-    const username = loginForm.username.value.trim();
-    const password = loginForm.password.value.trim();
+  // Collect values from the login form
+  const username = loginForm.username.value.trim();
+  const password = loginForm.password.value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        // document.location.replace('/');
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // document.location.replace('/');
 
-        // if (response.ok) {
-        //     document.location.replace('/homepage');
-        // } else {
-        //     alert(response.statusText);
-        // }
-    }
-    document.location.replace('/pokedex');
+    // if (response.ok) {
+    //     document.location.replace('/homepage');
+    // } else {
+    //     alert(response.statusText);
+    // }
+  }
+  document.location.replace('/pokedex');
 };
 
 //need to target the signup page
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ name, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+  if (name && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-            // document.location.replace('/homepage');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      // document.location.replace('/homepage');
+    } else {
+      alert(response.statusText);
     }
+  }
 };
 
-    // When the login button is clicked, the following code is executed
-loginButton.addEventListener("click", (e) => {
-    // Prevent the default submission of the form
-    e.preventDefault();
-    // Get the values input by the user in the form fields
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+// When the login button is clicked, the following code is executed
+loginButton.addEventListener('click', (e) => {
+  // Prevent the default submission of the form
+  e.preventDefault();
+  // Get the values input by the user in the form fields
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-    if (username && password ) {
-        // If the credentials are valid, show an alert box and reload the page
-        loginFormHandler(e);
-        alert("You have successfully logged in.");
-    } else {
-        // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username && password) {
+    // If the credentials are valid, show an alert box and reload the page
+    loginFormHandler(e);
+    alert('You have successfully logged in.');
+  } else {
+    // Otherwise, make the login error message show (change its oppacity)
+    loginErrorMsg.style.opacity = 1;
+  }
+});
--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -16,13 +16,13 @@ var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
-  }
-});
+//form.addEventListener('submit', function (e) {
+// e.preventDefault();
+// if (input.value) {
+//   socket.emit('chat message', input.value);
+//   input.value = '';
+// }
+//});
 
 // socket.on('chat message', function (msg) {
 //   var item = document.createElement('li');
@@ -31,20 +31,29 @@ form.addEventListener('submit', function (e) {
 //   window.scrollTo(0, document.body.scrollHeight);
 // });
 
-
-
 // constants and variables
 const TYPES = [
-  'normal', 'fighting', 'flying',
-  'poison', 'ground', 'rock',
-  'bug', 'ghost', 'steel',
-  'fire', 'water', 'grass',
-  'electric', 'psychic', 'ice',
-  'dragon', 'dark', 'fairy'
+  'normal',
+  'fighting',
+  'flying',
+  'poison',
+  'ground',
+  'rock',
+  'bug',
+  'ghost',
+  'steel',
+  'fire',
+  'water',
+  'grass',
+  'electric',
+  'psychic',
+  'ice',
+  'dragon',
+  'dark',
+  'fairy',
 ];
 let prevUrl = null;
 let nextUrl = null;
-
 
 // Functions
 const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
@@ -56,10 +65,11 @@ const resetScreen = () => {
   }
 };
 
-const fetchPokeList = url => {
+const fetchPokeList = (url) => {
   fetch(url)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
       const { results, previous, next } = data;
       prevUrl = previous;
       nextUrl = next;
@@ -80,10 +90,10 @@ const fetchPokeList = url => {
     });
 };
 
-const fetchPokeData = id => {
+const fetchPokeData = (id) => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       resetScreen();
 
       const dataTypes = data['types'];
@@ -130,14 +140,12 @@ const handleListItemClick = (e) => {
   fetchPokeData(id);
 };
 
-
 // adding event listeners
 leftButton.addEventListener('click', handleLeftButtonClick);
 rightButton.addEventListener('click', handleRightButtonClick);
 for (const pokeListItem of pokeListItems) {
   pokeListItem.addEventListener('click', handleListItemClick);
 }
-
 
 // initialize App
 fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
